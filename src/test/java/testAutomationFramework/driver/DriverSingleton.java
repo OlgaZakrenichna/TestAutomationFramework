ea8de1ebdb35d5ae1013fc9be1abc710a00a2c41
OlgaZakrenichna <olga.romashka@rambler.ru>
2022-06-12 15:13:12 +0300

    set windowSize

--- src/test/java/testAutomationFramework/driver/DriverSingleton.java
+++ src/test/java/testAutomationFramework/driver/DriverSingleton.java
@@ -3,6 +3,7 @@ package testAutomationFramework.driver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -27,7 +28,7 @@ public class DriverSingleton {
                 }
             }
             logger.info(driver.manage().window().getSize());
-            driver.manage().window().maximize();
+            driver.manage().window().setSize(new Dimension(1920, 980));
             logger.info(driver.manage().window().getSize());
         }
         return driver;
