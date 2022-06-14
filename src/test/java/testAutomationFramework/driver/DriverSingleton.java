818f5e24d571d7bf4a02cd60924205d07091a994
OlgaZakrenichna <olga.romashka@rambler.ru>
2022-06-14 22:43:46 +0300

    change switch

--- src/test/java/testAutomationFramework/driver/DriverSingleton.java
+++ src/test/java/testAutomationFramework/driver/DriverSingleton.java
@@ -3,10 +3,13 @@ package testAutomationFramework.driver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class DriverSingleton {
     private static WebDriver driver;
@@ -19,18 +22,26 @@ public class DriverSingleton {
             switch (System.getProperty("browser")) {
                 case "firefox": {
                     WebDriverManager.firefoxdriver().setup();
-                    driver = new FirefoxDriver();
+                    FirefoxOptions options = new FirefoxOptions();
+                    options.setHeadless(true);
+                    driver = new FirefoxDriver(options);
+                    driver.manage().window().setSize(new Dimension(1920, 10000));
+                    break;
                 }
-                default: {
+                case "chrome": {
                     WebDriverManager.chromedriver().setup();
                     ChromeOptions options = new ChromeOptions();
                     options.addArguments("--headless");
                     options.addArguments("--window-size=1920,1080");
                     driver = new ChromeDriver(options);
+                    break;
+                }
+                default: {
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    driver.manage().window().maximize();
                 }
             }
-//            logger.info(driver.manage().window().getSize());
-//            driver.manage().window().maximize();
             logger.info(driver.manage().window().getSize());
         }
         return driver;
